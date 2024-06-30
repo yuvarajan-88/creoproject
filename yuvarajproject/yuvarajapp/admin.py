7a913c7055aac3f397c251b59105d23b8a3a9fee
yuvarajan-88 <yuvarajany30@gmail.com>
2024-06-30 19:33:55 +0530

    Delete yuvarajproject directory

--- yuvarajproject/yuvarajapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Student, SubjectMarks
-
-admin.site.register(Student)
-admin.site.register(SubjectMarks)
--- yuvarajproject/yuvarajapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('student/<int:pk>/', views.student_detail, name='student_detail'),
-]
